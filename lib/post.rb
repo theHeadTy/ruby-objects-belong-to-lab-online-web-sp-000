2eac65eacd2c4fd763dcd33ec2eff0acb45dd006
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-14 03:42:08 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,21 +1,7 @@
-
-describe "Post" do
-  let!(:post) { Post.new } 
-  it "has a title" do
-    post.title = "Hello World" 
-    expect(post.title).to eq("Hello World")
-  end
-
-  it "belongs to an author" do 
-    author = Author.new
-    post.author = author
-    expect(post.author).to eq(author)
-  end
-
-  it "knows the name of the author it belongs to" do 
-    author = Author.new
-    author.name = "Uncle Bob"
-    post.author = author
-    expect(post.author.name).to eq("Uncle Bob")
-  end
+class Post < Author
+  attr_accessor :title, :author
 end
+
+post = Post.new
+post.title = 'Hello World'
+post.author = author
